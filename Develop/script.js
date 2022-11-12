e24df8c08a5bcf5342498ca417015d723772224c
Tyler Walton <tylerwalton300@gmail.com>
2022-11-11 21:37:43 -0500

    adding JS functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,65 @@
-$("#currentDay").text(today.format("MMM D, YYYY"));
+var today = dayjs();
+$("#currentDay").text(today.format("MMM D, YYYY H:mm"));
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var saveButtonEl = document.querySelector("save");
 var now = dayjs().hour();
-var hour9 = $("#hour9");
-var hour10 = $("#hour10");
-var hour11 = $("#hour11");
-var hour12 = $("#hour12");
-var hour13 = $("#hour13");
-var hour14 = $("#hour14");
-var hour15 = $("#hour15");
-var hour16 = $("#hour16");
-var hour17 = $("#hour17");
-var today = dayjs();
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour13 = $("#13");
+var hour14 = $("#14");
+var hour15 = $("#15");
+var hour16 = $("#16");
+var hour17 = $("#17");
 
-// // const dayJsObject = dayjs();
-// // console.log(dayJsObject.format('D/M/YYYY'))
 
 $(document).ready(function () {
-    // saveBtn click listener
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+  // saveBtn click listener
+  $(".saveBtn").on("click", function () {
+    // Get nearby values of the description in JQuery
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    })
+    // Save text in local storage
+    localStorage.setItem(time, text);
+  });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  $("#9 .description").val(localStorage.getItem("9"))
+  $("#10 .description").val((localStorage.getItem("10")))
+  $("#11 .description").val((localStorage.getItem("11")))
+  $("#12 .description").val((localStorage.getItem("12")))
+  $("#13 .description").val((localStorage.getItem("13")))
+  $("#14 .description").val((localStorage.getItem("14")))
+  $("#15 .description").val((localStorage.getItem("15")))
+  $("#16 .description").val((localStorage.getItem("16")))
+  $("#17 .description").val((localStorage.getItem("17")))
   
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
+$(".time-block").each(function(){
+var hour = dayjs().format("H")
+console.log(hour);
+var divId = parseInt($(this).attr("id"))
+if (divId<hour) {
+  $(this).removeClass("present")
+  $(this).removeClass("future")
+  $(this).addClass("past")
+}
+else if (divId === hour) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+}
+else {
+   $(this).removeClass("past");
+   $(this).removeClass("present");
+   $(this).addClass("future");
+}
+}) 
+
+
   // TODO: Add code to display the current date in the header of the page.
-
 });
